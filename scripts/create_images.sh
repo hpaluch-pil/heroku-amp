43ac6257b66a8c56dfbf25c2484c6068afc1eced
Henryk Paluch <henryk.paluch@pickering.cz>
2018-02-20 08:27:07 +0000

    Added generated logo

--- scripts/create_images.sh
+++ scripts/create_images.sh
@@ -4,10 +4,22 @@
 
 set -ex
 cd `dirname $0`
+
+# News Article image
 convert \
 	 -border 10 -bordercolor White \
 	 -background White \
          label:"My News Image" \
 	 ../assets/news-image.png
+
+
+# Publisher logo image - must be 600x60
+convert \
+	 -size 600x60 \
+	 -background White \
+         label:"My Publisher logo" \
+	 ../assets/logo.png
+
+
 exit 0
 
