8def56f9ef99f1d77f5ee1675d9804485d66b375
Henryk Paluch <henryk.paluch@pickering.cz>
2018-02-20 11:55:54 +0000

    Use current date

--- amp-test.php
+++ amp-test.php
@@ -28,7 +28,8 @@
 		}
 	 },
 	 "headline": "<?php echo $amp_headline; ?>",
-	"datePublished": "2015-10-07T12:02:41Z",
+	 "datePublished": "<?php echo date('c'); ?>",
+	 "dateModified": "<?php echo date('c'); ?>",
 	"image": [
 			{
 			"@type": "ImageObject",
@@ -43,7 +44,7 @@
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
   </head>
   <body>
-	<h1>Welcome to the AMP-mobile web</h1>
+  <h1><?php echo $amp_title; ?></h1>
 	<p>This is modified source from <a href="https://www.ampproject.org/docs/tutorials/create/basic_markup">AMP project</a> page.
         <p> Inline picture here:
 	  <amp-img src="<?php echo $images['news-image.png']['url']; ?>" alt="News Image"
